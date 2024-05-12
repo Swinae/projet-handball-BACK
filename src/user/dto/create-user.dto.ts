f331c5deb5aad9be14ce6cb9d6db7a5b7956fc3c
jordan <jordannigthwatch@hotmail.com>
2024-05-12 22:21:21 +0200

    create-user-dto.ts : delete key role / seedMainAdmin.ts : created a file to seed an admin

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,13 +2,11 @@ import { UserRoleEnum } from "@prisma/client";
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty({message: "Veuillez rentrer une adresse mail"})
-    email: string;
+  @IsEmail()
+  @IsNotEmpty({ message: "Veuillez rentrer une adresse mail" })
+  email: string;
 
-    @IsNotEmpty()
-    @MinLength(8, {message: "Le mot de passe doit contenir 8 caratères minimum"})
-    password: string;
-
-    role?: UserRoleEnum;
+  @IsNotEmpty()
+  @MinLength(8, { message: "Le mot de passe doit contenir 8 caratères minimum" })
+  password: string;
 }
