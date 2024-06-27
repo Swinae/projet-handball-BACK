e14ac306bc4109573d7b66c52eaeb7b6d3cc36a9
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-06-27 09:13:02 +0200

    Added : - matchs results CRUD

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
+import { PrismaService } from 'prisma/prisma.service';
+import { Match } from '@prisma/client';
 
 @Injectable()
 export class MatchService {
+  
+  constructor(private readonly prisma: PrismaService) { }
+  
   create(createMatchDto: CreateMatchDto) {
     return 'This action adds a new match';
   }
 
-  findAll() {
-    return `This action returns all match`;
+  async findAll() :Promise<Match[]> {
+    return this.prisma.match.findMany();
   }
 
   findOne(id: number) {
